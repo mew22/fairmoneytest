ed4e1e4e8ebe36f41968f96aa214723da98bf652
Sebastien <sebastien.delaherche@renault.com>
2021-09-28 21:01:40 +0200

    chore: add useful dependencies

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -18,6 +18,56 @@ object Dependency {
             const val coreKtx = "androidx.core:core-ktx:$VERSION"
         }
 
+        object Fragment {
+            private const val VERSION = "1.3.6"
+            const val fragment = "androidx.fragment:fragment-ktx:$VERSION"
+        }
+
+        object Navigation {
+            private const val VERSION = "2.3.5"
+            const val navigationFragment =
+                "androidx.navigation:navigation-fragment-ktx:$VERSION"
+            const val navigationUi = "androidx.navigation:navigation-ui-ktx:$VERSION"
+        }
+
+        object Recyclerview {
+            private const val VERSION = "1.2.0"
+            const val recyclerview = "androidx.recyclerview:recyclerview:$VERSION"
+        }
+
+        object SwipeRefreshLayout {
+            private const val VERSION = "1.1.0"
+            const val refresh = "androidx.swiperefreshlayout:swiperefreshlayout:$VERSION"
+        }
+
+        object Paging {
+            private const val VERSION = "3.0.1"
+            const val common = "androidx.paging:paging-common-ktx:$VERSION"
+            const val runtime = "androidx.paging:paging-runtime-ktx:$VERSION"
+        }
+
+        object Room {
+            private const val VERSION = "2.3.0"
+            const val room = "androidx.room:room-ktx:$VERSION"
+            const val compiler = "androidx.room:room-compiler:$VERSION"
+        }
+
+        object LifeCycle {
+            private const val VERSION = "2.2.0"
+            private const val VERSION_RUNTIME = "2.4.0-beta01"
+
+            // use version beta to profit of repeatOnLifecycle convenient api
+            const val lifecycleExt = "androidx.lifecycle:lifecycle-extensions:$VERSION"
+            const val livedata = "androidx.lifecycle:lifecycle-livedata-ktx:$$VERSION"
+            const val lifecycleVm = "androidx.lifecycle:lifecycle-viewmodel-ktx:$VERSION"
+            const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:$VERSION_RUNTIME"
+        }
+
+        object Splashscreen {
+            private const val VERSION = "1.0.0-alpha01"
+            const val splash = "androidx.core:core-splashscreen:$VERSION"
+        }
+
         object TestExt {
             private const val VERSION = "1.1.3"
             const val junit = "androidx.test.ext:junit:$VERSION"
@@ -35,6 +85,15 @@ object Dependency {
         }
     }
 
+    object KotlinX {
+        object Coroutines {
+            private const val VERSION = "1.5.2"
+            const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VERSION"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VERSION"
+            const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$VERSION"
+        }
+    }
+
     object Config {
         const val applicationId = "com.sdelaherche.fairmoneytest"
         const val compileSdk = 31
@@ -45,15 +104,33 @@ object Dependency {
         const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    object GradlePlugin {
-        object Kotlin {
-            private const val VERSION = "1.5.30"
-            const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VERSION"
+    object SquareUp {
+        object Retrofit {
+            private const val VERSION = "2.9.0"
+            const val retrofit = "com.squareup.retrofit2:retrofit:$VERSION"
+            const val gsonConverter =
+                "com.squareup.retrofit2:converter-gson:$VERSION"
+            const val mock = "com.squareup.retrofit2:retrofit-mock:$VERSION"
         }
-        object Android {
-            private const val VERSION = "7.0.2"
-            const val plugin = "com.android.tools.build:gradle:$VERSION"
+
+        object Okhttp {
+            private const val VERSION = "4.9.1"
+            private const val MOCK_VERSION = "5.0.0-alpha.2"
+            const val interceptor =
+                "com.squareup.okhttp3:logging-interceptor:$VERSION"
+            const val mockWebServer = "com.squareup.okhttp3:mockwebserver3-junit5:$MOCK_VERSION"
         }
+    }
+
+    object Koin {
+        private const val VERSION = "3.1.2"
+        const val koin = "io.insert-koin:koin-core:$VERSION"
+        const val android = "io.insert-koin:koin-android:$VERSION"
+    }
+
+    object Glide {
+        private const val VERSION = "4.12.0"
+        const val glide = "com.github.bumptech.glide:glide:$VERSION"
     }
 
     object Google {
