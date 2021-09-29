ca4d8e424ff9d6620c79f988a6b33eaddd8c2b3c
Sebastien <sebastien.delaherche@renault.com>
2021-09-29 14:29:59 +0200

    feat: add user list dependency injection

---
+++ app/src/main/java/com/sdelaherche/fairmoneytest/FairMoneyTestApplication.kt
@@ -0,0 +1,18 @@
+package com.sdelaherche.fairmoneytest
+
+import android.app.Application
+import com.sdelaherche.fairmoneytest.common.di.commonModule
+import com.sdelaherche.fairmoneytest.userlist.di.userListModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class FairMoneyTestApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@FairMoneyTestApplication)
+            modules(modules = listOf(commonModule, userListModule))
+        }
+    }
+}
---
+++ app/src/main/java/com/sdelaherche/fairmoneytest/userlist/di/Module.kt
@@ -0,0 +1,39 @@
+@file:JvmName("UserListModule")
+
+package com.sdelaherche.fairmoneytest.userlist.di
+
+import com.sdelaherche.fairmoneytest.userlist.data.remote.IUserRemoteService
+import com.sdelaherche.fairmoneytest.userlist.data.repository.UserRepository
+import com.sdelaherche.fairmoneytest.userlist.domain.repository.IUserRepository
+import com.sdelaherche.fairmoneytest.userlist.domain.usecase.GetUserListUseCase
+import com.sdelaherche.fairmoneytest.userlist.domain.usecase.RefreshUserListUseCase
+import com.sdelaherche.fairmoneytest.userlist.presentation.UserListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+
+val userListModule = module {
+    single { get<Retrofit>().create(IUserRemoteService::class.java) }
+
+    single<IUserRepository> {
+        UserRepository(
+            remoteSource = get(),
+            localSource = get()
+        )
+    }
+
+    factory {
+        GetUserListUseCase(userRepository = get())
+    }
+
+    factory {
+        RefreshUserListUseCase(userRepository = get())
+    }
+
+    viewModel {
+        UserListViewModel(
+            getUserListUseCase = get<GetUserListUseCase>(),
+            refreshListUseCase = get<RefreshUserListUseCase>()
+        )
+    }
+}
