cc9d8527794f8d5413e67f218bc9150b40dac21c
Sebastien <sebastien.delaherche@renault.com>
2021-09-29 14:53:04 +0200

    feat: add user detail dependency injection

--- app/src/main/java/com/sdelaherche/fairmoneytest/FairMoneyTestApplication.kt
+++ app/src/main/java/com/sdelaherche/fairmoneytest/FairMoneyTestApplication.kt
@@ -2,6 +2,7 @@ package com.sdelaherche.fairmoneytest
 
 import android.app.Application
 import com.sdelaherche.fairmoneytest.common.di.commonModule
+import com.sdelaherche.fairmoneytest.userdetail.di.userDetailModule
 import com.sdelaherche.fairmoneytest.userlist.di.userListModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -12,7 +13,7 @@ class FairMoneyTestApplication : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@FairMoneyTestApplication)
-            modules(modules = listOf(commonModule, userListModule))
+            modules(modules = listOf(commonModule, userListModule, userDetailModule))
         }
     }
 }
