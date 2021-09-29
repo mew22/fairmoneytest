37815c43af5c16924007594fdba6e88931e61743
Sebastien <sebastien.delaherche@renault.com>
2021-09-29 14:53:04 +0200

    feat: add user detail dependency injection

---
+++ app/src/main/java/com/sdelaherche/fairmoneytest/userdetail/di/Module.kt
@@ -0,0 +1,39 @@
+@file:JvmName("UserDetailModule")
+
+package com.sdelaherche.fairmoneytest.userdetail.di
+
+import com.sdelaherche.fairmoneytest.userdetail.data.remote.IUserDetailRemoteService
+import com.sdelaherche.fairmoneytest.userdetail.data.repository.UserDetailRepository
+import com.sdelaherche.fairmoneytest.userdetail.domain.repository.IUserDetailRepository
+import com.sdelaherche.fairmoneytest.userdetail.domain.usecase.GetUserDetailUseCase
+import com.sdelaherche.fairmoneytest.userdetail.domain.usecase.RefreshUserDetailUseCase
+import com.sdelaherche.fairmoneytest.userdetail.presentation.UserDetailViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+
+val userDetailModule = module {
+    single { get<Retrofit>().create(IUserDetailRemoteService::class.java) }
+
+    single<IUserDetailRepository> {
+        UserDetailRepository(
+            remoteSource = get(),
+            localSource = get()
+        )
+    }
+
+    factory {
+        GetUserDetailUseCase(userDetailRepository = get())
+    }
+
+    factory {
+        RefreshUserDetailUseCase(userDetailRepository = get())
+    }
+
+    viewModel {
+        UserDetailViewModel(
+            getUserUseCase = get<GetUserDetailUseCase>(),
+            refreshUseCase = get<RefreshUserDetailUseCase>()
+        )
+    }
+}
